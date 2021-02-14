94d5b446cdd8641aeb412298bf4c5f5a9b6c1c0c
Joe Freeman <joe@freemansoft.com>
2021-02-14 16:24:46 -0500

    better data file download handling

--- start-kaggle-container.sh
+++ start-kaggle-container.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # get the data from our world of data
-mkdir data
-wget -P data "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"
-#curl --create-dirs  https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv --output data/vaccinations.csv
-
 script_dir=`dirname "$0"`
+remote_source_file="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv"
+dest_data_file="$script_dir/data/vaccinations.csv"
 
-if [ ! -f $script_dir/data/country_vaccinations.csv ] ; then
-    echo " you need to download the data set into a ./data directory from https://www.kaggle.com/gpreda/covid-world-vaccination-progress"
-    exit 1
+
+if [ ! -f $dest_data_file ] ; then
+    echo "Downloading data file from $remote_source_file"
+    mkdir data
+    #wget -P $script_dir/data "$remote_source_file"
+    curl --create-dirs $remote_source_file --output $dest_data_file
 fi
 
 docker run --rm -it -v $PWD:/home/jupyter -p 8080:8080 --env NOTEBOOK_DISABLE_ROOT=true gcr.io/kaggle-images/python /run_jupyter.sh 
